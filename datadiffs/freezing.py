eb6dce9e5988ebea6610d6c089fa4bb509684102
Andrzej Pragacz <apragacz@o2.pl>
2016-07-20 18:12:58 +0200

    new frozendict interface

--- datadiffs/freezing.py
+++ datadiffs/freezing.py
@@ -1,42 +1,58 @@
+from collections import Mapping
 
 
-class frozendict(dict):
+class frozendict(Mapping):
+
+    def __init__(self, input):
+        if not isinstance(input, dict):
+            raise TypeError('{0} is not type of dict'.format(type(input)))
+        self.__dict = input.copy()
+
+    def __getitem__(self, key):
+        return self.__dict[key]
+
+    def __len__(self):
+        return len(self.__dict)
+
+    def __iter__(self):
+        return iter(self.__dict)
+
+    def __contains__(self, item):
+        return item in self.__dict
 
     def __hash__(self):
         h = 0
-        for item in self.items():
+        for item in self.__dict.items():
             h += hash(item)
         return h
 
-    def __setitem__(self, key, value):
-        raise NotImplementedError()
-
     def __repr__(self):
-        return '{0}({1!r})'.format(self.__class__.__name__, super().copy())
+        return '{0}({1!r})'.format(self.__class__.__name__, self.__dict)
+
+    def items(self):
+        return self.__dict.items()
+
+    def keys(self):
+        return self.__dict.keys()
+
+    def values(self):
+        return self.__dict.values()
+
+    def get(self, key, default=None):
+        return self.__dict.get(key, default)
+
+    def put(self, key, value):
+        copy = self.__dict.copy()
+        copy[key] = value
+        return self.__class__(copy)
+
+    def delete(self, key):
+        copy = self.__dict.copy()
+        del copy[key]
+        return self.__class__(copy)
 
     def copy(self):
-        return frozendict(super().copy())
-
-    def pop(self, *args, **kwargs):
-        d = super().copy()
-        v = d.pop(*args, **kwargs)
-        return frozendict(d), v
-
-    def popitem(self, *args, **kwargs):
-        d = super().copy()
-        item = d.popitem(*args, **kwargs)
-        return frozendict(d), item
-
-    def update(self, E, **F):
-        d = super().copy()
-        d.update(E, **F)
-        return frozendict(d)
-
-    def clear(self):
-        raise NotImplementedError()
-
-    def setdefault(self, k, d):
-        raise NotImplementedError()
+        return self.__class__(self.__dict.copy())
 
 
 def freeze_data(data):
--- tests/test_frozendict.py
+++ tests/test_frozendict.py
@@ -1,46 +1,31 @@
-import pytest
 from datadiffs.freezing import frozendict
 
 
-def test_not_implemented():
-    fd = frozendict({'a': 1})
-    with pytest.raises(NotImplementedError):
-        fd.setdefault('a', 2)
-    with pytest.raises(NotImplementedError):
-        fd.clear()
-    with pytest.raises(NotImplementedError):
-        fd['b'] = 3
-
-
-def test_copy():
-    fd1 = frozendict({'a': 1})
-    fd2 = fd1.copy()
-    assert fd1 is not fd2
-    assert fd1 == fd2
-    assert isinstance(fd2, frozendict)
-
-
-def test_pop():
+def test_getitem():
     fd1 = frozendict({'a': 1, 'b': 3})
-    fd2, v = fd1.pop('a')
-    assert fd1 == frozendict({'a': 1, 'b': 3})
-    assert fd2 == frozendict({'b': 3})
-    assert v == 1
+    assert fd1['a'] == 1
+    assert fd1['b'] == 3
 
 
-def test_popitem():
+def test_len():
+    fd0 = frozendict({})
     fd1 = frozendict({'a': 1, 'b': 3})
-    fd2, item = fd1.popitem()
-    assert fd1 == frozendict({'a': 1, 'b': 3})
-    assert len(fd2) == len(fd1) - 1
-    assert item in (('a', 1), ('b', 3))
+    assert len(fd0) == 0
+    assert not bool(fd0)
+    assert len(fd1) == 2
+    assert bool(fd1)
 
 
-def test_update():
+def test_iter():
     fd1 = frozendict({'a': 1, 'b': 3})
-    fd2 = fd1.update({'b': 2, 'c': 4})
-    assert fd1 == frozendict({'a': 1, 'b': 3})
-    assert fd2 == frozendict({'a': 1, 'b': 2, 'c': 4})
+    assert set(fd1) == {'a', 'b'}
+
+
+def test_contains():
+    fd1 = frozendict({'a': 1, 'b': 3})
+    assert 'a' in fd1
+    assert 'b' in fd1
+    assert 'c' not in fd1
 
 
 def test_hash():
@@ -52,3 +37,54 @@ def test_hash():
 def test_repr():
     fd = frozendict({'a': 1, 'b': 3})
     assert repr(fd).startswith('frozendict')
+
+
+def test_items():
+    fd1 = frozendict({'a': 1, 'b': 3})
+    assert sorted(fd1.items()) == [('a', 1), ('b', 3)]
+
+
+def test_keys():
+    fd1 = frozendict({'a': 1, 'b': 3})
+    assert set(fd1.keys()) == {'a', 'b'}
+
+
+def test_values():
+    fd1 = frozendict({'a': 1, 'b': 3})
+    assert sorted(fd1.values()) == [1, 3]
+
+
+def test_get():
+    fd1 = frozendict({'a': 1, 'b': 3})
+    assert fd1.get('a', 4) == 1
+    assert fd1.get('b', 4) == 3
+    assert fd1.get('c', 4) == 4
+    assert fd1.get('a') == 1
+    assert fd1.get('b') == 3
+    assert fd1.get('c') is None
+
+
+def test_put():
+    fd1 = frozendict({'a': 1, 'b': 3})
+    fd2 = fd1.put('a', 2)
+    fd3 = fd1.put('c', 2)
+    assert fd1 != fd2
+    assert fd1 != fd3
+    assert fd1 == frozendict({'a': 1, 'b': 3})
+    assert fd2 == frozendict({'a': 2, 'b': 3})
+    assert fd3 == frozendict({'a': 1, 'b': 3, 'c': 2})
+
+
+def test_delete():
+    fd1 = frozendict({'a': 1})
+    fd2 = fd1.delete('a')
+    assert fd1 != fd2
+    assert fd2 == frozendict({})
+
+
+def test_copy():
+    fd1 = frozendict({'a': 1})
+    fd2 = fd1.copy()
+    assert fd1 is not fd2
+    assert fd1 == fd2
+    assert isinstance(fd2, frozendict)
